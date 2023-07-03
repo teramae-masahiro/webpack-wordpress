779e7567efc965d3fa2f6ed210db7decc032b965
masahiro <mmaassaa0618@gmail.com>
2023-07-03 10:23:21 +0900

    complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,2 @@
-import '../scss/styles.scss';
+import '../scss/app.scss';
+console.log('hekk');
--- src/news/news.php
+++ src/news/news.php
@@ -2,24 +2,15 @@
 <html lang="ja">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="format-detection" content="telephone=no">
-  <meta name="viewport" content="width=device-width">
-  <link rel="apple-touch-icon" href="/img/common/apple-touch-icon.png">
-  <title><?php echo $title; ?></title>
-  <meta name="description" content="<?php echo $description; ?>">
-  <meta property="og:title" content="<?php echo $title; ?>">
-  <meta property="og:description" content="<?php echo $description; ?>">
-  <meta property="og:url" content="<?php echo $siteData["siteInfo"]["siteUrl"] . $siteData["pages"][$pageId]["slug"]; ?>">
-  <meta property="og:type" content="<?php echo $siteData["pages"][$pageId]["type"]; ?>">
-  <meta property="og:local" content="ja_JP">
-  <meta property="og:image" content="/common/og-image.jpg">
-  <meta property="og:site_name" content="<?php echo $siteData["siteInfo"]["siteUrl"]; ?> ">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
   <link rel="stylesheet" type="text/css" href="/css/main.css">
 </head>
 
 <body>
-  <script src="../js/index.js"></script>
+
+  <script src="./js/index.js"></script>
 </body>
 
 </html>
